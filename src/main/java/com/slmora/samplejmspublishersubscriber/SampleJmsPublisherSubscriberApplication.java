d816355b54c4fc0fbba4b20aa01bcc4f98a8790d
UmeshGunasekara <umesh.gunasekara.n@gmail.com>
2021-06-10 17:38:06 +0800

    MORALEARNLENOVO - master - update - SLMORA - UTC202106100938

---
+++ src/main/java/com/slmora/samplejmspublishersubscriber/SampleJmsPublisherSubscriberApplication.java
@@ -0,0 +1,15 @@
+package com.slmora.samplejmspublishersubscriber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJmsPublisherSubscriberApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SampleJmsPublisherSubscriberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/slmora/samplejmspublishersubscriber/SampleJmsPublisherSubscriberApplicationTests.java
@@ -0,0 +1,15 @@
+package com.slmora.samplejmspublishersubscriber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleJmsPublisherSubscriberApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
